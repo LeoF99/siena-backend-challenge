9e3651740a14dfc2bef7a226084caf31996e8c6e
LeoF99 <leo.ferreira@dcx.ufpb.br>
2024-04-21 01:27:46 -0300

    test: fix test dependency

--- src/domain/fileUpload/test/fileUpload.service.spec.ts
+++ src/domain/fileUpload/test/fileUpload.service.spec.ts
@@ -2,16 +2,26 @@ import { Express } from 'express';
 import S3Provider from '../../../infrastructure/aws/s3Provider/s3Provider';
 import FileUploadService from '../fileUpload.service';
 import envVars from '../../../config/envVars';
+import MessageBrokerProvider from '../../../infrastructure/messageBroker/messageBrokerProvider';
+import MessagesService from '../../messages/messages.service';
 
 describe('FileUploadService', () => {
   let fileUploadService: FileUploadService;
   let s3Provider: S3Provider;
+  let messageBrokerProvider: MessageBrokerProvider;
+  let messagesService: MessagesService;
   let multerFile: Express.Multer.File;
 
   beforeEach(() => {
     envVars.application.CSV_MINIMUM_SIZE = 1;
     s3Provider = new S3Provider();
-    fileUploadService = new FileUploadService(s3Provider);
+    messageBrokerProvider = new MessageBrokerProvider();
+    messagesService = new MessagesService(s3Provider);
+    fileUploadService = new FileUploadService(
+      s3Provider,
+      messageBrokerProvider,
+      messagesService,
+    );
     multerFile = {
       buffer: Buffer.from(
         'sender_username,reciever_username,message,channel\ntest,test,test,instagram',
